225b07c53364ec75901cfa7257d687e664409dd2
tonyb983sPC <tonyb983@gmail.com>
2019-10-21 17:53:07 -0400

    create store methods

--- src/Models/Blog/StoreModel.spec.js
+++ src/Models/Blog/StoreModel.spec.js
@@ -1,14 +1,29 @@
 import {
   Store,
-  createFreshStore,
+  createEmptyStore,
   createPreloadedStore,
   createStoreFromSnapshot,
+  createRandomStore,
+  CreateRandomPost,
+  CreateRandomUser,
+  CreateBlog,
+  CreateUserStore
 } from './StoreModel';
+import { isString } from 'lodash';
 
 describe('Store Model Tests', () => {
   it('Can create a new Store', () => {
     const store = Store.create({});
 
     expect(store).toBeDefined();
+  });
+});
+
+describe('Create Random Functions', () => {
+  it('Can create a random User', () => {
+    const user = CreateRandomUser();
+    expect(user).toBeDefined();
+    expect(isString(user.login)).toBe(true);
+    console.log(`Random User: ${JSON.stringify(user, null, 2)}`);
   });
 });
